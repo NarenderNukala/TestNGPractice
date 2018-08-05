87b921f621df97e6a05c0f3953365df11784da01
Narender <narendernukala@gmail.com>
2018-08-05 03:47:58 -0700

    Changes made to programs

---
+++ TestNG/src/TestNGExample.java
@@ -0,0 +1,18 @@
+import org.testng.annotations.Test;
+
+public class TestNGExample {
+	
+	 //working
+		// test case 1
+		@Test
+		public void testCase1() {
+			System.out.println("in test case 1");
+		}
+
+		// test case 2
+		@Test
+		public void testCase2() {
+			System.out.println("in test case 2");
+		}
+
+}
