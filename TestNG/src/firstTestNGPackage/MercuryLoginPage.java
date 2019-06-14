aa1144bd553c97cf776627c0238b1e71489f9c1d
Narender <narendernukala@gmail.com>
2019-06-15 04:15:50 +0530

    Commiting Local changes

--- TestNG/src/firstTestNGPackage/MercuryLoginPage.java
+++ TestNG/src/firstTestNGPackage/MercuryLoginPage.java
@@ -7,7 +7,6 @@ import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
